9e2daa9d7a114ce81bb316bd9738a030eb1ac845
LukkaJo <jonatanlukkarinen26@gmail.com>
2026-02-04 16:58:45 +0200

    I created a test class for connecting to a database, the password and username need to be changed from the URL. JL.

---
+++ src/main/java/com/gradle/SmartWaste/MongoClientConnectionExample.java
@@ -0,0 +1,40 @@
+package com.gradle.SmartWaste;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class MongoClientConnectionExample {
+    public static void main(String[] args) {
+        //TODO Need to change password and user to get access database.
+        String connectionString = "mongodb+srv://<db_username>:<db_password>@cluster0.ldvviwi.mongodb.net/?appName=Cluster0";
+
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
