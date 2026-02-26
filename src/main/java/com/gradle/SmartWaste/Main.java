7e2e4683fca49a49f4935e16dd74b63ab7119fb8
Wirttis <95577610+Wirttis@users.noreply.github.com>
2026-02-26 19:06:40 +0200

    Delete old testclass and replace with new improved Main that starts program

---
+++ src/main/java/com/gradle/SmartWaste/Main.java
@@ -0,0 +1,12 @@
+package com.gradle.SmartWaste;
+import com.gradle.MQTT.*;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+public class Main {
+    public static void main(String[] args) throws MqttException {
+        MessageHandler messageHandler = new MessageHandler();
+        MqttReceiver mqttReceiver = new MqttReceiver(messageHandler);
+        mqttReceiver.start();
+
+    }
+}
