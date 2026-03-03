8141032c89fc80c1f9b61688671db343233d8554
Wirttis <95577610+Wirttis@users.noreply.github.com>
2026-03-03 18:23:27 +0200

    added messagehandler usage to main

--- src/main/java/com/gradle/SmartWaste/Main.java
+++ src/main/java/com/gradle/SmartWaste/Main.java
@@ -1,14 +1,15 @@
 package com.gradle.SmartWaste;
+import com.example.MessageHandler;
 import com.example.MongoHandler;
 import com.gradle.MQTT.*;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class Main {
     public static void main(String[] args) throws MqttException {
+        MongoHandler mongoHandler = new MongoHandler();
         MessageHandler messageHandler = new MessageHandler();
         MqttReceiver mqttReceiver = new MqttReceiver(messageHandler);
         mqttReceiver.start();
-        MongoHandler mongoHandler = new MongoHandler();
         mongoHandler.connect();
     }
 }
