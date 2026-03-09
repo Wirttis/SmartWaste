6f1e4f77ac594767404e1a09144af3a6ae22444f
Wirttis <95577610+Wirttis@users.noreply.github.com>
2026-03-09 08:32:12 +0200

    changed starting order of applications

--- src/main/java/com/gradle/SmartWaste/Main.java
+++ src/main/java/com/gradle/SmartWaste/Main.java
@@ -5,18 +5,20 @@ import com.gradle.MQTT.*;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
 // main method, using Spring Boot
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class Main {
     static MongoHandler mongoHandler;
     public static void main(String[] args) throws MqttException {
         // starts everything including mqtt client, mongo client and spring boot app
-        SpringApplication.run(Main.class, args);
         mongoHandler = new MongoHandler();
         mongoHandler.connect();
         MessageHandler messageHandler = new MessageHandler(mongoHandler);
         MqttReceiver mqttReceiver = new MqttReceiver(messageHandler);
         mqttReceiver.start();
+        SpringApplication.run(Main.class, args);
     }
 }
