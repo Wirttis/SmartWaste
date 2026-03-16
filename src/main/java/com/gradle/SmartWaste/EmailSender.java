272d1ed0844811055cf7b7f244a6a1738555656e
Wirttis <95577610+Wirttis@users.noreply.github.com>
2026-03-16 20:32:24 +0200

    EmailSender class to send emails

---
+++ src/main/java/com/gradle/SmartWaste/EmailSender.java
@@ -0,0 +1,51 @@
+package com.gradle.SmartWaste;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.*;
+import java.util.Properties;
+
+public class EmailSender {
+
+    private static final String FROM_EMAIL = System.getenv("EMAIL");
+    private static final String PASSWORD = System.getenv("EMAIL_PASSWORD");
+
+    // Create SMTP Session
+    public static Session createSession() {
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(FROM_EMAIL, PASSWORD);
+            }
+        });
+    }
+
+    // Send Email Method
+    public static void sendEmail(String toEmail, String subject, String messageText) {
+
+        try {
+            System.out.println("Sending email to: " + toEmail);
+            Session session = createSession();
+            System.out.println("test success");
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(FROM_EMAIL));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(toEmail));
+
+            message.setSubject(subject);
+            message.setText(messageText);
+
+            Transport.send(message);
+
+            System.out.println("Email sent successfully!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
