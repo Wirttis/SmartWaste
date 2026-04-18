17156a6fde5b0c39cbbde87e1d821f96b6cf7e07
Wirttis <95577610+Wirttis@users.noreply.github.com>
2026-04-18 19:25:58 +0300

    get method for all containers or single containers get method for all subscribers or single subscriber post method to add subscriber changed mapping path

--- src/main/java/com/gradle/SmartWaste/Controller.java
+++ src/main/java/com/gradle/SmartWaste/Controller.java
@@ -1,21 +1,43 @@
 package com.gradle.SmartWaste;
 
-import com.example.MongoListener;
-import org.bson.Document;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/containers")
 public class Controller {
+    private SubscriberService subscriberService;
+    private ContainerService containerService;
+
+    @Autowired
+    public Controller(SubscriberService subscriberService, ContainerService containerService) {
+        this.subscriberService = subscriberService;
+        this.containerService = containerService;
+    }
+
     @CrossOrigin
-    @GetMapping("/api/v1/test")
-    public List<Document> test(){
-        MongoListener mongoListener = new MongoListener(Main.mongoHandler);
-        return  mongoListener.getLatestContainers();
+    @GetMapping("/container")
+    public List<Container> getAllContainers(){
+        containerService.updateContainers();
+        return  containerService.getAllContainers();
+    }
+    @GetMapping("/container/{id}")
+    public Optional<Container> getContainer(@PathVariable String id){
+        containerService.updateContainers();
+        return containerService.getContainer(Integer.parseInt(id));
+    }
+    @GetMapping("/subscriber")
+    public List<Subscriber> getSubs(){
+        return subscriberService.getAllSubscribers();
+    }
+    @GetMapping("/subscriber/{id}")
+    public Optional<Subscriber> getSubscriber(@PathVariable String id){
+        return subscriberService.getSubscriber(Integer.parseInt(id));
+    }
+    @PostMapping("/subscriber")
+    public void saveSubscriber(@RequestBody Subscriber subscriber){
+        subscriberService.add(subscriber);
     }
 }
